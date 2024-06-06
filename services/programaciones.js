e1221f7d0a41c6966bdbe5dfe6b0c4f7ea74759d
gabrielVargas611 <gavargas77@gmail.com>
2024-06-05 21:01:34 -0600

    programaciones.js
    Creation of the service programaciones.js

---
+++ services/programaciones.js
@@ -0,0 +1,76 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+class Programaciones {
+  constructor() {}
+
+  async Agregar(Programacion) {
+    let resultado;
+    try {
+      resultado = await prisma.programaciones.create({
+        data: {
+          fechaInicioDisponible: FechaInicio,
+          fechaFinalDisponible: FechaFinal,
+          IdDelServicio: Servicio,
+        },
+      });
+    } catch (error) {
+      console.error(
+        `No se pudo crear la programacion ${Programacion} debido al error: ${error}`
+      );
+    }
+    return resultado;
+  }
+
+  async Actualizar(programacionId, Programacion) {
+    let resultado;
+    try {
+      resultado = await prisma.programaciones.update({
+        where: { programacionId: parseInt(programacionId) },
+        data: {
+            fechaInicioDisponible: FechaInicio,
+            fechaFinalDisponible: FechaFinal,
+            IdDelServicio: Servicio,
+        },
+      });
+    } catch (error) {
+      console.error(
+        `No se pudo actualizar la programacion ${Programacion} debido al error: ${error}`
+      );
+    }
+    return resultado;
+  }
+
+  async Borrar(programacionId) {
+    let resultado;
+    try {
+      resultado = await prisma.programaciones.delete({
+        where: {
+            programacionId: parseInt(programacionId),
+        },
+      });
+    } catch (error) {
+      console.error(
+        `No se pudo eliminar la programacion ${Programacion} debido al error: ${error}`
+      );
+    }
+    return resultado;
+  }
+
+  Listar(programacionId) {
+    let programaciones;
+    if (programacionId === undefined) {
+        programaciones = prisma.programaciones.findMany();
+    } else {
+        programaciones = prisma.programaciones.findMany({
+        where: {
+            programacionId: parseInt(programacionId),
+        },
+      });
+    }
+    return programaciones;
+  }
+}
+
+module.exports = Programaciones;
