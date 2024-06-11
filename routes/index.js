c6f6ffacbe9462b5bb13bb4bdebf45162272f790
gabrielVargas611 <gavargas77@gmail.com>
2024-06-10 20:49:56 -0600

    Routes Creation
    creation of the app routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 
 
 function routerAPI(app){
-    app.use('/',);
+    //Ruta Programaciones
+    app.use('/Programaciones',);
+
+    //Ruta Servicios
+    app.use('/Servicios',);
+
+    //Ruta Solicitantes
+    app.use('/Solicitantes',);
+    
+    //Ruta Solicitudes
+    app.use('/Solicitudes',);
+
+    //Ruta Usuarios
+    app.use('/Usuarios',);
 }
+
 
 module.exports = routerAPI;
