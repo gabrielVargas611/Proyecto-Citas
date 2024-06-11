4c7dde6c853ac248b6af45d9afac6f32c919bd9f
gabrielVargas611 <gavargas77@gmail.com>
2024-06-10 21:37:43 -0600

    General Fixes
    General fixes and updates

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const routerAPI = require('./routes/index.js')
 
 const app = express();
 app.use(express.json());
+routerAPI(app);
 app.listen(80, () => {
   console.log("Servidor iniciado en el puerto 80");
 });
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,28 @@
+const routerprogramaciones = require('./programaciones.js');
 
+const routerservicios = require('./servicios.js');
+
+const routersolicitantes = require('./solicitantes.js');
+
+const routersolicitudes = require('./solicitudes.js');
+
+const routerusuarios = require('./usuarios.js');
 
 function routerAPI(app){
     //Ruta Programaciones
-    app.use('/Programaciones',);
+    app.use('/Programaciones', routerprogramaciones);
 
     //Ruta Servicios
-    app.use('/Servicios',);
+    app.use('/Servicios', routerservicios);
 
     //Ruta Solicitantes
-    app.use('/Solicitantes',);
+    app.use('/Solicitantes',routersolicitantes);
     
     //Ruta Solicitudes
-    app.use('/Solicitudes',);
+    app.use('/Solicitudes',routersolicitudes);
 
     //Ruta Usuarios
-    app.use('/Usuarios',);
+    app.use('/Usuarios',routerusuarios);
 }
 
 
